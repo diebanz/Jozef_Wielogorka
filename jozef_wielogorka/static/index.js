fc3a95a9c10c18ac5cdbe6849c82c6fac2055aa3
Sebastian on Mac <sebastian.banz@deliveryhero.com>
2021-11-18 09:35:15 +0100

    google maps update (still missing api key)

--- jozef_wielogorka/static/index.js
+++ jozef_wielogorka/static/index.js
@@ -1,8 +1,13 @@
 function myMap() {
+    const store = { lat: 51.5, lng: -0.12 };
     let mapOptions = {
-        center: new google.maps.LatLng(51.5, -0.12),
+        center: store,
         zoom: 10,
         mapTypeId: google.maps.MapTypeId.HYBRID,
     };
     let map = new google.maps.Map(document.getElementById("map"), mapOptions);
+    const marker = new google.maps.Marker({
+        position: store,
+        map: map,
+      });    
 }
