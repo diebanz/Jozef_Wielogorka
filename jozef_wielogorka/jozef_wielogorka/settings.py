0bf5c099d8d3b6fb65f8d1f1313ddfaa6d0552b9
Sebastian on Mac <sebastian.banz@deliveryhero.com>
2021-11-19 07:20:01 +0100

    Email and map update.

--- jozef_wielogorka/jozef_wielogorka/settings.py
+++ jozef_wielogorka/jozef_wielogorka/settings.py
@@ -187,3 +187,11 @@ MARKDOWNIFY = {
     }
 }
 
+# Contact Email settings
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+DEFAULT_FROM_EMAIL = 'noreply<no_reply@sebanzian.com>'
--- jozef_wielogorka/news/views.py
+++ jozef_wielogorka/news/views.py
@@ -1,12 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.core.mail import EmailMessage
+from django.contrib import messages
+from django.http import HttpResponseRedirect
 
 from .models import News
+from .forms import ContactForm
 
 
 # Create your views here.
 def home(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email_address = form.cleaned_data['email']
+            message = form.cleaned_data['message']
+            email_1 = EmailMessage(
+                f'New message on <website> by {email_address}',
+                message,
+                to=['sebanzian@gmail.com'],
+                from_email='System',
+            )
+            email_1.send()
+            message = f"Hello {name},\n\nWe received your contact request, thank you for reaching out. We will try and answer within 72 hours.\n\nBest regards,\nJozef Wielogorka"
+            email_2 = EmailMessage(
+                'Contact request sent',
+                message,
+                to=[email_address],
+                from_email='no_reply'
+            )
+            email_2.send()
+            messages.success(
+                request,
+                'Your message has been sent.'
+            )
+            return HttpResponseRedirect('/#')
+        else:
+            messages.error(
+                request,
+                'Unsuccessful, please try again.'
+            )
+    form = ContactForm()
     current = News.objects.all().order_by('-id')[0]
     context = {
+        'form': form,
         'news': current,
     }
     return render(request, 'index.html', context)
