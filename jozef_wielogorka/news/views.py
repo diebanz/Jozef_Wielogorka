4430583a0c2ae31a936f4550b2bf8e3e2cf661d1
Sebastian on Mac <sebastian.banz@deliveryhero.com>
2021-11-29 17:04:35 +0100

    Logging

--- jozef_wielogorka/news/views.py
+++ jozef_wielogorka/news/views.py
@@ -14,7 +14,8 @@ def home(request):
         if form.is_valid():
             name = form.cleaned_data['name']
             email_address = form.cleaned_data['email']
-            message = form.cleaned_data['message'] + f'\n\n<a href="mailto:{email_address}?body={message}">Odpowiedz tutaj</a>'
+            message = form.cleaned_data['message'] + 'test' + f'\n\n<a href="mailto:{email_address}?body={message}">Odpowiedz tutaj</a>'
+            # message = form.cleaned_date['message']
             email_1 = EmailMessage(
                 f'Nowa wiadomość na wielomiod.pl napisana przez {email_address}',
                 message,
