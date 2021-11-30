09c38600232b1fb556fe203bef56b6a98337310f
Sebastian on Mac <sebastian.banz@deliveryhero.com>
2021-11-30 09:44:25 +0100

    Working Emails

--- jozef_wielogorka/news/views.py
+++ jozef_wielogorka/news/views.py
@@ -14,8 +14,13 @@ def home(request):
         if form.is_valid():
             name = form.cleaned_data['name']
             email_address = form.cleaned_data['email']
-            msg = form.cleaned_data['message']
-            email_1 = create_mail_to_owner(msg, email_address, name)
+            message = form.cleaned_data['message']
+            email_1 = EmailMessage(
+                'Nowa wiadomość',
+                message,
+                to=['jozek@wielomiod.pl'],
+                from_email=email_address
+            )
             email_1.send()
             message = f"Hej {name},\nOtrzymałem Twoją prośbę o kontakt. Dziękuję ! Postaram się odpowiedzieć w ciągu najbliższych 72 godzin.\n\nPozdrawiam,\nJózek Wielogorka"
             email_2 = EmailMessage(
@@ -41,47 +46,3 @@ def home(request):
         'news': current,
     }
     return render(request, 'index.html', context)
-
-
-def create_mail_to_owner(msg, target, target_name):
-    special_characters = {
-        ' ': '%20',
-        '!': '%21',
-        '"': '%22',
-        '#': '%23',
-        '$': '%24',
-        '%': '%25',
-        '&': '%26',
-        '\'': '%27',
-        '(': '%28',
-        ')': '%29',
-        '*': '2A',
-        '+': '2B',
-        ',': '2C',
-        '-': '2D',
-        '.': '2E',
-        '/': '2F',
-        ':': '%3A',
-        ';': '%3B',
-        '<': '%3C',
-        '=': '%3D',
-        '>': '%3E',
-        '?': '%3F',
-        '[': '%5B',
-        '\\': '%5C',
-        ']': '%5D',
-        '^': '%5E',
-        '_': '%5F',
-        '`': '%60',
-        '{': '%7B',
-        '|': '%7C',
-        '}': '%7D',
-        '~': '%7E',
-        '€': '%E2%82%AC',
-    }
-    translation_table = msg.maketrans(special_characters)
-    return EmailMessage(
-                f'Nowa wiadomość na wielomiod.pl napisana przez {target}',
-                f'Dzień dobry {target_name},\n\n\n' + f'mailto:{{{target}}}?subject={{Re%3AYour%20Request}}&{{{msg.translate(translation_table)}}}'',
-                to=['jozek@wielomiod.pl'],
-            )
